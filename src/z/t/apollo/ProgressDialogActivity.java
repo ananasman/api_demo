dd712df166fba4cabcf37d01bc04e91c6d8302fe
AnanasMan <songbl@live.com>
2014-09-10 12:20:11 +0800

    7

---
+++ src/z/t/apollo/ProgressDialogActivity.java
@@ -0,0 +1,41 @@
+package z.t.apollo;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class ProgressDialogActivity extends Activity {
+	private Button button;
+	private ProgressDialog progressDialog;
+	public static final String ACTIOIN = "z.t.apollo.ProgressDialogActivity";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_progress_dialog);
+		button = (Button) findViewById(R.id.btnshowprogressdialog);
+		button.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				progressDialog = ProgressDialog.show(getApplicationContext(),
+						"wait", "......");
+				new Thread() {
+					@Override
+					public void run() {
+						try {
+							Thread.sleep(1000);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+						progressDialog.dismiss();
+						super.run();
+					}
+				};
+			}
+		});
+	}
+}
