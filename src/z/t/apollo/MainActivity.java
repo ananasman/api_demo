638f816426931192c60fec928cf08ebd50836dc4
AnanasMan <songbl@live.com>
2014-09-11 16:42:04 +0800

    更新

--- src/z/t/apollo/MainActivity.java
+++ src/z/t/apollo/MainActivity.java
@@ -6,10 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends ListActivity {
 	private ArrayAdapter<ListCellData> adapter;
-//	public static final String ACTION = "z.t.apollo.MainActivity";
+
+	// public static final String ACTION = "z.t.apollo.MainActivity";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -40,4 +42,21 @@ public class MainActivity extends ListActivity {
 		super.onListItemClick(l, v, position, id);
 	}
 
+	private long clicktime = 0;
+
+	@Override
+	public void onBackPressed() {
+		if (clicktime <= 0) {
+			Toast.makeText(this, "再按一下退出", Toast.LENGTH_SHORT).show();
+			clicktime = System.currentTimeMillis();
+		}else{
+			long currentTime = System.currentTimeMillis();
+			if(currentTime-clicktime<1000){
+				finish();
+			}else{
+				Toast.makeText(this, "再按一下退出", Toast.LENGTH_SHORT).show();
+				clicktime=currentTime;
+			}
+		}
+	}
 }
